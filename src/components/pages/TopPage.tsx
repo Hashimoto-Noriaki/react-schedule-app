409132a6e87657ed19fdb34312ef138bc058b9a9
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-01-06 00:30:13 +0900

    トップページ1

---
+++ src/components/pages/TopPage.tsx
@@ -0,0 +1,17 @@
+export const TopPage = () => {
+  return (
+    <div>
+        <header>
+            <div className="flex justify-between">
+                <p>スケジュール管理</p>
+                <nav>
+                    <ul className="flex gap-3">
+                        <li>ログイン</li>
+                        <li>利用説明</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </div>
+  )
+}
