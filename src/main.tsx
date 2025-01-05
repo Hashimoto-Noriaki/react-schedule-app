7833692bcf4d74e8e6e5aade22eca14c27176691
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-01-05 22:22:37 +0900

    tailwindの設定2

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
+import "./styles/output.css"
 import "./styles/destyle.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode></React.StrictMode>
 )
