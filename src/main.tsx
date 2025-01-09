cc624a23bf0b0adaaf578adff35e698af7f7053c
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-01-09 22:39:55 +0900

    練習のために削除

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,8 @@ import './styles/index.css'
 import "./styles/output.css"
 import "./styles/destyle.css";
 
-// import { TopPage } from "./components/pages/TopPage";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <TopPage /> */}
   </React.StrictMode>
 )
