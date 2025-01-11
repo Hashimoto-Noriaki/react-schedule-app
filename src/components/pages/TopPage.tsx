d07fdf35cd00c16396b6e2efa5974d3fbbeba380
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-01-12 00:41:30 +0900

    トップページ作成

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -12,22 +12,15 @@ export const TopPage = () => {
             </nav>
           </div>
         </header>
-        <main className="pt-[50px] flex flex-col justify-center items-center">
-          <div className="text-center">
-            <h1 className="text-7xl logo">スケジュール管理App</h1>
-            <p className="pt-[10vh] text-5xl">スケジュール管理します。</p>
-            <div className="pt-[20vh]">
-              {/* ログインボタン */}
-              <button className="bg-lime-800 text-white p-4 rounded-lg text-lg">
-                ログイン
-              </button>
-              {/* グラデーション枠線ボタン */}
-              <button className="p-1 bg-gradient-to-r from-lime-500 to-orange-500 rounded-lg mt-10">
-                <div className="bg-white rounded-lg p-4">
-                  グラデーション枠線のボックス
-                </div>
-              </button>
-            </div>
+        <main className="pt-[50px] bg-gradient-to-r from-lime-200 to-lime-300 h-screen">
+          <div className="text-center pt-[10vh]">
+            <h1 className="logo text-7xl">スケジュール管理App</h1>
+            <p className="text-5xl pt-[10vh]">スケジュール管理します。</p>
+          </div>
+          <div className="text-center pt-[10vh]">
+            <button className="bg-lime-800 text-white p-4 text-lg rounded-lg">
+              ログイン
+            </button>
           </div>
         </main>
       </div>
